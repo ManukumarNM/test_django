5bb897bdec184c0cab4e445a23284c71317e7e40
Manukumar N M <manunm.jps@gmail.com>
2022-03-14 15:07:37 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/models.py
+++ monitor/openwisp_monitoring/device/models.py
@@ -9,9 +9,9 @@ BaseDevice = load_model_patched('config', 'Device', require_ready=False)
 
 class DeviceData(AbstractDeviceData, BaseDevice):
     checks = GenericRelation(get_model_name('check', 'Check'))
-    metrics = GenericRelation(get_model_name('monitoring', 'Metric'))
+   # metrics = GenericRelation(get_model_name('monitoring', 'Metric'))
 
-    class Meta:
+   # class Meta:
       #  proxy = True
        # swappable = swappable_setting('device_monitoring', 'DeviceData')
 
