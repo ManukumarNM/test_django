2286e6332b8e66324dd08024f23b8e5da6e5aaef
Manukumar N M <manunm.jps@gmail.com>
2022-03-14 15:08:00 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/models.py
+++ monitor/openwisp_monitoring/device/models.py
@@ -7,8 +7,8 @@ from .base.models import AbstractDeviceData, AbstractDeviceMonitoring
 BaseDevice = load_model_patched('config', 'Device', require_ready=False)
 
 
-class DeviceData(AbstractDeviceData, BaseDevice):
-    checks = GenericRelation(get_model_name('check', 'Check'))
+#class DeviceData(AbstractDeviceData, BaseDevice):
+   # checks = GenericRelation(get_model_name('check', 'Check'))
    # metrics = GenericRelation(get_model_name('monitoring', 'Metric'))
 
    # class Meta:
