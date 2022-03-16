14afbf38398e88a25e2a956d998d6600d862918c
Manukumar N M <manunm.jps@gmail.com>
2022-03-16 17:41:10 +0530

    Update signals.py

--- monitor/openwisp_monitoring/monitoring/signals.py
+++ monitor/openwisp_monitoring/monitoring/signals.py
@@ -2,11 +2,11 @@
 
 #threshold_crossed = Signal()
 #threshold_crossed.__doc__ = """
-Providing arguments: ['metric', 'alert_settings', 'target', 'first_time']
+#Providing arguments: ['metric', 'alert_settings', 'target', 'first_time']
 """
 pre_metric_write = Signal()
 pre_metric_write.__doc__ = """
-Providing arguments: ['metric', 'values', 'time', 'current']
+#Providing arguments: ['metric', 'values', 'time', 'current']
 """
 post_metric_write = Signal()
 post_metric_write.__doc__ = """
