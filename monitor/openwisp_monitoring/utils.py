a820be6c2658740c898cd1bea3822999d7f245e5
Manukumar N M <manunm.jps@gmail.com>
2022-03-16 17:38:59 +0530

    Update utils.py

--- monitor/openwisp_monitoring/utils.py
+++ monitor/openwisp_monitoring/utils.py
@@ -26,11 +26,11 @@
   #  return apps.get_model(app_label, model, require_ready=require_ready)
 
 
-def retry(method):
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        max_retries = MONITORING_TIMESERIES_RETRY_OPTIONS.get('max_retries')
-        delay = MONITORING_TIMESERIES_RETRY_OPTIONS.get('delay')
+#def retry(method):
+ #   @wraps(method)
+#    def wrapper(*args, **kwargs):
+#        max_retries = MONITORING_TIMESERIES_RETRY_OPTIONS.get('max_retries')
+#        delay = MONITORING_TIMESERIES_RETRY_OPTIONS.get('delay')
         for attempt_no in range(1, max_retries + 1):
             try:
                 return method(*args, **kwargs)
