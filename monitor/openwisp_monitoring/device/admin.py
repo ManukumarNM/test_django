f6027a79cd79daaf6935a74ad73c8c9cbb590446
Manukumar N M <manunm.jps@gmail.com>
2022-03-31 16:25:29 +0530

    Update admin.py

--- monitor/openwisp_monitoring/device/admin.py
+++ monitor/openwisp_monitoring/device/admin.py
@@ -5,9 +5,9 @@
 #from django.contrib.contenttypes.admin import GenericStackedInline
 #from django.contrib.contenttypes.forms import BaseGenericInlineFormSet
 #from django.contrib.contenttypes.models import ContentType
-from django.forms import ModelForm
-from django.urls import reverse
-from django.utils.html import format_html
+#from django.forms import ModelForm
+#from django.urls import reverse
+#from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from nested_admin.nested import (
