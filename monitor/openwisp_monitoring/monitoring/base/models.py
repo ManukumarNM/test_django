3146457d12acf0843ab013d98051e22f49c50111
Manukumar N M <manunm.jps@gmail.com>
2022-04-12 17:13:36 +0530

    Update models.py

--- monitor/openwisp_monitoring/monitoring/base/models.py
+++ monitor/openwisp_monitoring/monitoring/base/models.py
@@ -5,8 +5,8 @@
 #from dateutil.parser import parse as parse_date
 #from django.conf import settings
 #from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+#from django.contrib.contenttypes.fields import GenericForeignKey
+#from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.validators import MaxValueValidator
 from django.db import models
