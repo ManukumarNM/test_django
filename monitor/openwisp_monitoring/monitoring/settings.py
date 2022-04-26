82e1b52b54db57d78575bf1fc9935b264a2a5f0d
Manukumar N M <manunm.jps@gmail.com>
2022-04-26 14:39:24 +0530

    Update settings.py

--- monitor/openwisp_monitoring/monitoring/settings.py
+++ monitor/openwisp_monitoring/monitoring/settings.py
@@ -1,9 +1,9 @@
 #from ..settings import get_settings_value
 
 #ADDITIONAL_CHARTS = get_settings_value('CHARTS', {})
-ADDITIONAL_METRICS = get_settings_value('METRICS', {})
+#ADDITIONAL_METRICS = get_settings_value('METRICS', {})
 
-RETRY_OPTIONS = get_settings_value(
+#RETRY_OPTIONS = get_settings_value(
     'WRITE_RETRY_OPTIONS',
     dict(
         max_retries=None, retry_backoff=True, retry_backoff_max=600, retry_jitter=True
