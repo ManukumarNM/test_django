938f448dddc55c00c518fafc8556acba4040d5f5
Manukumar N M <manunm.jps@gmail.com>
2022-05-12 14:35:30 +0530

    Update settings.py

--- monitor/openwisp_monitoring/settings.py
+++ monitor/openwisp_monitoring/settings.py
@@ -5,10 +5,10 @@
     #return getattr(settings, f'OPENWISP_MONITORING_{option}', default)
 
 
-AUTO_CHARTS = get_settings_value(
-    'AUTO_CHARTS',
-    (
-        'traffic',
+#AUTO_CHARTS = get_settings_value(
+    #'AUTO_CHARTS',
+    #(
+        #'traffic',
         'wifi_clients',
         'uptime',
         'packet_loss',
