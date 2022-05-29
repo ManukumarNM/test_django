34faf9e5fefbc9df698a1c3f6a4aaafd0efad77b
Manukumar N M <manunm.jps@gmail.com>
2022-05-29 21:04:23 +0530

    Update signals.py

--- monitor/openwisp_monitoring/device/signals.py
+++ monitor/openwisp_monitoring/device/signals.py
@@ -1,6 +1,6 @@
-from django.dispatch import Signal
+#from django.dispatch import Signal
 
-health_status_changed = Signal()
+#health_status_changed = Signal()
 health_status_changed.__doc__ = """
 Providing arguments: ['instance', 'status']
 """
