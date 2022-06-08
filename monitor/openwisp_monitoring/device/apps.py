a229c0377c3ba108b476612c9d2615ba00474cb9
Manukumar N M <manunm.jps@gmail.com>
2022-06-08 22:22:52 +0530

    Update apps.py

--- monitor/openwisp_monitoring/device/apps.py
+++ monitor/openwisp_monitoring/device/apps.py
@@ -20,7 +20,7 @@
 #from ..settings import MONITORING_API_BASEURL, MONITORING_API_URLCONF
 #from ..utils import transaction_on_commit
 #from . import settings as app_settings
-from .signals import device_metrics_received, health_status_changed
+#from .signals import device_metrics_received, health_status_changed
 #from .utils import get_device_cache_key, manage_short_retention_policy
 
 
