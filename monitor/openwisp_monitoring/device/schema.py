8b91ab1a2e8234bf2328bf33836899a8bc6bfa5d
Manukumar N M <manunm.jps@gmail.com>
2022-07-06 13:02:52 +0530

    Update schema.py

--- monitor/openwisp_monitoring/device/schema.py
+++ monitor/openwisp_monitoring/device/schema.py
@@ -367,7 +367,7 @@
                                     "anyOf": [{"format": "ipv4"}, {"format": "ipv6"}],
                                 },
                                 "mask": {"type": "integer"},
-                            },
+                         #   },
                       #  },
                   #  },
                # },
