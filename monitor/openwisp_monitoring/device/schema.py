bf12bb11e0849bb9edcee9c079863a1a0d376ef9
Manukumar N M <manunm.jps@gmail.com>
2022-07-12 09:14:09 +0530

    Update schema.py

--- monitor/openwisp_monitoring/device/schema.py
+++ monitor/openwisp_monitoring/device/schema.py
@@ -131,7 +131,7 @@
                         #"total": {"type": "integer"},
                       #  "free": {"type": "integer"},
                   #  },
-                },
+               # },
             },
         },
         "interfaces": {
