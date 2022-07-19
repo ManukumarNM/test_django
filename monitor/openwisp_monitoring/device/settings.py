6b203eeb98c31a6520ebaf2375353174128f1376
Manukumar N M <manunm.jps@gmail.com>
2022-07-19 17:33:12 +0530

    Update settings.py

--- monitor/openwisp_monitoring/device/settings.py
+++ monitor/openwisp_monitoring/device/settings.py
@@ -43,7 +43,7 @@
 
 
 #SHORT_RETENTION_POLICY = get_settings_value('SHORT_RETENTION_POLICY', '24h0m0s')
-CRITICAL_DEVICE_METRICS = get_critical_device_metrics()
+#CRITICAL_DEVICE_METRICS = get_critical_device_metrics()
 HEALTH_STATUS_LABELS = get_health_status_labels()
 AUTO_CLEAR_MANAGEMENT_IP = get_settings_value('AUTO_CLEAR_MANAGEMENT_IP', True)
 # Triggers spontaneous recovery of device based on corresponding signals
