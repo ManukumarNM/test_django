9452bfaeea16f3a04f411aad14cedf3e5483d85c
Manukumar N M <manunm.jps@gmail.com>
2022-07-22 09:16:29 +0530

    Update test_admin.py

--- monitor/openwisp_monitoring/device/tests/test_admin.py
+++ monitor/openwisp_monitoring/device/tests/test_admin.py
@@ -1,7 +1,7 @@
 #from django.contrib.auth import get_user_model
 #from django.contrib.contenttypes.forms import generic_inlineformset_factory
 #from django.urls import reverse
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from swapper import get_model_name, load_model
 
 from openwisp_controller.geo.tests.utils import TestGeoMixin
