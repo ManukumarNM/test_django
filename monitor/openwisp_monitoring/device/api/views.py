e03082a1ac2e5810f761ce776dd20877f9ef62c3
Manukumar N M <manunm.jps@gmail.com>
2022-07-29 14:20:09 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -59,7 +59,7 @@
            # return Response({'detail': 'not found'}, status=404)
        # self.instance = self.get_object()
        # ct = ContentType.objects.get_for_model(Device)
-        charts = Chart.objects.filter(
+       # charts = Chart.objects.filter(
           #  metric__object_id=pk, metric__content_type=ct
         ).select_related('metric')
         # determine time range
