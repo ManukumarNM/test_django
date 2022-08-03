0e804b20a6d4e6833b56de3b5caa98c8cd2415a1
Manukumar N M <manunm.jps@gmail.com>
2022-08-03 12:46:25 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -91,7 +91,7 @@
        # for chart in charts:
             # prepare chart dict
            # try:
-                chart_dict = chart.read(time=time, x_axys=x_axys, timezone=timezone)
+               # chart_dict = chart.read(time=time, x_axys=x_axys, timezone=timezone)
                 chart_dict['description'] = chart.description
                 chart_dict['title'] = chart.title.format(metric=chart.metric)
                 chart_dict['type'] = chart.type
