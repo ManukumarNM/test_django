f25f726e50a75d37eb3fd457d94634d45d58b467
Manukumar N M <manunm.jps@gmail.com>
2022-08-11 09:37:56 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -167,7 +167,7 @@
        # except ValidationError as e:
            # logger.info(e.message)
            # return Response(e.message, status=status.HTTP_400_BAD_REQUEST)
-        time_obj = request.query_params.get(
+       # time_obj = request.query_params.get(
             'time', now().utcnow().strftime('%d-%m-%Y_%H:%M:%S.%f')
         )
         current = request.query_params.get('current', False)
