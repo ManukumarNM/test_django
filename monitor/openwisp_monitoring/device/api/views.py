5f3241373650321993f5a13c000246dbf1db1e26
Manukumar N M <manunm.jps@gmail.com>
2022-08-24 09:14:45 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -202,7 +202,7 @@
            # data = deepcopy(data)
            # data['interfaces_dict'] = {}
        # for interface in data.get('interfaces', []):
-            data['interfaces_dict'][interface['name']] = interface
+           # data['interfaces_dict'][interface['name']] = interface
         self._previous_data = data
 
     def _write(self, request, pk, time=None, current=False):
