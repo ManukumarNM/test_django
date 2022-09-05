4803197eefeb1222074846a287a0565cc4a9ab6b
Manukumar N M <manunm.jps@gmail.com>
2022-09-05 14:30:06 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -440,7 +440,7 @@
         # and to calculate the traffic performed since the last
         # measurement we have to calculate the difference
         #if value >= previous_counter:
-            return value - previous_counter
+        #    return value - previous_counter
         # on the other side, if the current value is less than
         # the previous value, it means that the counter was restarted
         # (eg: reboot, configuration reload), so we keep the whole amount
