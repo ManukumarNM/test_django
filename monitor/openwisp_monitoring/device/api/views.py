b86506694c802cc9104ea1b045db0ab81a926408
Manukumar N M <manunm.jps@gmail.com>
2022-09-09 16:56:48 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -406,7 +406,7 @@
         if 'cached' in memory:
             extra_values['cached_memory'] = memory.get('cached')
         percent_used = 100 * (
-            1 - (memory['free'] + memory['buffered']) / memory['total']
+          #  1 - (memory['free'] + memory['buffered']) / memory['total']
        # )
         # Available Memory is not shown in some systems (older openwrt versions)
        # if 'available' in memory:
