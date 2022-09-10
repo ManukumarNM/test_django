24acd343185f74bb0fed53d18acfb5b949347a5a
Manukumar N M <manunm.jps@gmail.com>
2022-09-10 15:35:20 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -365,7 +365,7 @@
     ):
       #  extra_values = {
           #  'load_1': float(load[0]),
-            'load_5': float(load[1]),
+          #  'load_5': float(load[1]),
             'load_15': float(load[2]),
         }
         metric, created = Metric._get_or_create(
