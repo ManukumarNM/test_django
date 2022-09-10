830a665508745a3a892ae5d3cbc8cbd424d9fa04
Manukumar N M <manunm.jps@gmail.com>
2022-09-10 15:36:03 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -367,7 +367,7 @@
           #  'load_1': float(load[0]),
           #  'load_5': float(load[1]),
           #  'load_15': float(load[2]),
-        }
+      #  }
         metric, created = Metric._get_or_create(
             object_id=primary_key, content_type=content_type, configuration='cpu'
         )
