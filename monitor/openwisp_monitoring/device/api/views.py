3599b848fe4ab45b39157d80509d3a91f0ce7f28
Manukumar N M <manunm.jps@gmail.com>
2022-09-27 17:23:56 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -376,7 +376,7 @@
            # self._create_resources_alert_settings(metric, resource='cpu')
       #  metric.write(
             #100 * float(load[0] / cpus), current, time=time, extra_values=extra_values
-        )
+       # )
 
     def _write_disk(
         self, disk_list, primary_key, content_type, current=False, time=None
