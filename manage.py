32fd2d28f2aac38a3c3c4395f052d0b654e83c09
Manukumar N M <manunm.jps@gmail.com>
2022-10-05 19:37:55 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 #import os
 ### """Run administrative tasks."""
-   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_django.settings')
+  # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_django.settings')
    from django.core.management import execute_from_command_line
   #  except ImportError as exc:
       ##  ) from exc
