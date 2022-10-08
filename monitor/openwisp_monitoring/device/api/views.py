b524005d9cc6d7978ead371cef17be0ce45ec7d6
Manukumar N M <manunm.jps@gmail.com>
2022-10-08 21:56:36 +0530

    Update views.py

--- monitor/openwisp_monitoring/device/api/views.py
+++ monitor/openwisp_monitoring/device/api/views.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from io import StringIO
 
 from django.conf import settings
-#from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.models import ContentType
 #from django.core.exceptions import ValidationError
 #from django.db.models import Count, Q
 #from django.http import HttpResponse
