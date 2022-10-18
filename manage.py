3ad9117ffafa72f0effeea30f0d7e0b0a2db7e50
Manukumar N M <manunm.jps@gmail.com>
2022-10-18 18:16:57 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@
   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_django.settings')
   from django.core.management import execute_from_command_line
   except ImportError as exc:
-      ##  ) from exc
+        ) from exc
    # execute_from_command_line(sys.argv)
 
 
