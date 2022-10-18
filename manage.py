3847ad38c7bf0938276c26ec7f5fe77ce1cc59f8
Manukumar N M <manunm.jps@gmail.com>
2022-10-18 18:17:25 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-#import os
+import os
 ### """Run administrative tasks."""
   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_django.settings')
   from django.core.management import execute_from_command_line
