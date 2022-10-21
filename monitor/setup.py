4493c6017c0adb11710b225d80c887c35d8283d5
Manukumar N M <manunm.jps@gmail.com>
2022-10-21 09:05:55 +0530

    Update setup.py

--- monitor/setup.py
+++ monitor/setup.py
@@ -30,7 +30,7 @@
     # delete any *.pyc, *.pyo and __pycache__
    # os.system('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
    # os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload -s dist/*")
+    #os.system("twine upload -s dist/*")
     os.system("rm -rf dist build")
     args = {'version': get_version()}
     print("You probably want to also tag the version now:")
