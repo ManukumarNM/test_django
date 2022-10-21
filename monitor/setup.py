6e5025b0b5819075af0c29245a09a36a073a1f7a
Manukumar N M <manunm.jps@gmail.com>
2022-10-21 09:03:54 +0530

    Update setup.py

--- monitor/setup.py
+++ monitor/setup.py
@@ -11,7 +11,7 @@
     """
     parse requirements.txt, ignore links, exclude comments
     """
-    requirements = []
+    #requirements = []
     for line in open('requirements.txt').readlines():
         # skip to next iteration if comment or empty line
         if (
