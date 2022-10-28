cf1eba79a692dd3b14a75e96883182ceb38762ec
Manukumar N M <manunm.jps@gmail.com>
2022-10-28 15:19:56 +0530

    Update models.py

--- monitor/openwisp_monitoring/check/base/models.py
+++ monitor/openwisp_monitoring/check/base/models.py
@@ -10,7 +10,7 @@
 
 #from openwisp_monitoring.check import settings as app_settings
 #from openwisp_monitoring.check.tasks import auto_create_config_check, auto_create_ping
-from openwisp_utils.base import TimeStampedEditableModel
+#from openwisp_utils.base import TimeStampedEditableModel
 
 from ...utils import transaction_on_commit
 
