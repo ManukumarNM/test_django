8cbc23ed7576887c4db031d4de15d617e00ebefb
Manukumar N M <manunm.jps@gmail.com>
2022-10-28 15:19:18 +0530

    Update models.py

--- monitor/openwisp_monitoring/check/base/models.py
+++ monitor/openwisp_monitoring/check/base/models.py
@@ -4,7 +4,7 @@
 #from django.contrib.contenttypes.models import ContentType
 #from django.db import models
 #from django.utils.functional import cached_property
-from django.utils.module_loading import import_string
+#from django.utils.module_loading import import_string
 from django.utils.translation import gettext_lazy as _
 from jsonfield import JSONField
 
