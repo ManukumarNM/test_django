27d9a7d701062a667945478ab5b7c183fa2954fc
Manukumar N M <manunm.jps@gmail.com>
2022-10-30 18:22:53 +0530

    Update models.py

--- monitor/openwisp_monitoring/check/base/models.py
+++ monitor/openwisp_monitoring/check/base/models.py
@@ -28,7 +28,7 @@
    # description = models.TextField(blank=True, help_text=_('Notes'))
    # content_type = models.ForeignKey(
        # ContentType, on_delete=models.CASCADE, null=True, blank=True
-    )
+   # )
     object_id = models.CharField(max_length=36, db_index=True, blank=True)
     content_object = GenericForeignKey('content_type', 'object_id')
     check_type = models.CharField(
