a93ea9029379a7bf7846e164fe99236841fed08a
Manukumar N M <manunm.jps@gmail.com>
2022-11-10 17:24:46 +0530

    Update base.py

--- monitor/openwisp_monitoring/check/classes/base.py
+++ monitor/openwisp_monitoring/check/classes/base.py
@@ -1,4 +1,4 @@
-from django.contrib.contenttypes.models import ContentType
+#from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from swapper import load_model
 
