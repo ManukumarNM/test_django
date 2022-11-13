18033777bef31675e55f209c82999a0dc8ed2d17
Manukumar N M <manunm.jps@gmail.com>
2022-11-13 12:38:36 +0530

    Update base.py

--- monitor/openwisp_monitoring/check/classes/base.py
+++ monitor/openwisp_monitoring/check/classes/base.py
@@ -34,7 +34,7 @@
         """
         Gets or creates metric
         """
-        check = self.check_instance
+       # check = self.check_instance
         if check.object_id and check.content_type:
             obj_id = check.object_id
             ct = check.content_type
