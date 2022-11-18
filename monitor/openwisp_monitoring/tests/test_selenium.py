a60c572a5790dbdd09692d53393cc7a45683decf
Manukumar N M <manunm.jps@gmail.com>
2022-11-18 17:57:46 +0530

    Update test_selenium.py

--- monitor/openwisp_monitoring/tests/test_selenium.py
+++ monitor/openwisp_monitoring/tests/test_selenium.py
@@ -3,7 +3,7 @@
 #from django.urls.base import reverse
 #from reversion.models import Version
 #from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException
+#from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
