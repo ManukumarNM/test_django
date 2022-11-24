daf8e94c2bc9f674f019b81fbcba738f64b7728a
Manukumar N M <manunm.jps@gmail.com>
2022-11-24 17:23:49 +0530

    Update client.py

--- monitor/openwisp_monitoring/db/backends/influxdb/client.py
+++ monitor/openwisp_monitoring/db/backends/influxdb/client.py
@@ -6,7 +6,7 @@
 
 #from django.conf import settings
 #from django.core.exceptions import ValidationError
-from django.utils.functional import cached_property
+#from django.utils.functional import cached_property
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from influxdb import InfluxDBClient
