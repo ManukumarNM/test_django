cb7d4f9d81d7cd6b178edd19f328395ba7980336
Manukumar N M <manunm.jps@gmail.com>
2022-12-01 16:23:50 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -104,7 +104,7 @@
        # if self.__data:
            # return self.__data
         #q = device_data_query.format(SHORT_RP, self.__key, self.pk)
-        cache_key = get_device_cache_key(device=self, context='current-data')
+        #cache_key = get_device_cache_key(device=self, context='current-data')
         points = cache.get(cache_key)
         #if not points:
            # points = timeseries_db.get_list_query(q, precision=None)
