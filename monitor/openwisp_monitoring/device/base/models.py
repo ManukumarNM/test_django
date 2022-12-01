732a4964f9a58ce5756920e8b4b2223563242967
Manukumar N M <manunm.jps@gmail.com>
2022-12-01 16:20:53 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -87,7 +87,7 @@
             # convert to GHz
            # if 'wireless' in interface and 'frequency' in interface['wireless']:
                 #interface['wireless']['frequency'] /= 1000
-            interface_dict[interface['name']] = interface
+            #interface_dict[interface['name']] = interface
         # reorder interfaces in alphabetical order
         interface_dict = OrderedDict(sorted(interface_dict.items()))
         data['interfaces'] = list(interface_dict.values())
