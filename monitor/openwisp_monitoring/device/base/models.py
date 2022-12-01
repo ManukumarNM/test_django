731f8cb23de1f911a18e13a44ec006071c34eb5e
Manukumar N M <manunm.jps@gmail.com>
2022-12-01 16:22:58 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -92,7 +92,7 @@
        # interface_dict = OrderedDict(sorted(interface_dict.items()))
         #data['interfaces'] = list(interface_dict.values())
         # reformat expiry in dhcp leases
-        for lease in data.get('dhcp_leases', []):
+        #for lease in data.get('dhcp_leases', []):
            # lease['expiry'] = datetime.fromtimestamp(lease['expiry'], tz=tz('UTC'))
         return data
 
