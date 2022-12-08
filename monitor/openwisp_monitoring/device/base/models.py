72f70f3c3004c02df63a9a99144b25cfa00a59a2
Manukumar N M <manunm.jps@gmail.com>
2022-12-08 18:00:32 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -161,7 +161,7 @@
                         signal_values[key] = float(value)
             # add mac vendor to wireless clients if present
            # if (
-                not mac_detection
+               #not mac_detection
                 or 'wireless' not in interface
                 or 'clients' not in interface['wireless']
             ):
