d8a129d298e34c905e54f6057f1ff5fa182ca6ab
Manukumar N M <manunm.jps@gmail.com>
2022-12-18 20:20:56 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -236,7 +236,7 @@
             '"{3}" means the device is not reachable or in critical conditions;'
         ).format(
             app_settings.HEALTH_STATUS_LABELS['unknown'],
-            app_settings.HEALTH_STATUS_LABELS['ok'],
+           # app_settings.HEALTH_STATUS_LABELS['ok'],
            # app_settings.HEALTH_STATUS_LABELS['problem'],
             #app_settings.HEALTH_STATUS_LABELS['critical'],
         ),
