ba788360db614352fa6c144cad10a13f7211bba4
Manukumar N M <manunm.jps@gmail.com>
2022-12-27 17:40:06 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -224,7 +224,7 @@
        # ('unknown', _(app_settings.HEALTH_STATUS_LABELS['unknown'])),
        # ('ok', _(app_settings.HEALTH_STATUS_LABELS['ok'])),
        # ('problem', _(app_settings.HEALTH_STATUS_LABELS['problem'])),
-        ('critical', _(app_settings.HEALTH_STATUS_LABELS['critical'])),
+      #  ('critical', _(app_settings.HEALTH_STATUS_LABELS['critical'])),
     )
     status = StatusField(
         _('health status'),
