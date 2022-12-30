99d61b1ae485e974741b01cd88dc8dbf40f91955
Manukumar N M <manunm.jps@gmail.com>
2022-12-30 09:07:01 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -254,7 +254,7 @@
         self.save()
         # clear device management_ip when device is offline
         #if self.status == 'critical' and app_settings.AUTO_CLEAR_MANAGEMENT_IP:
-            self.device.management_ip = None
+           # self.device.management_ip = None
            # self.device.save(update_fields=['management_ip'])
 
         health_status_changed.send(sender=self.__class__, instance=self, status=value)
