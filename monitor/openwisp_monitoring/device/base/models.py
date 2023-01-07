c31a4057b41a211e15bd530263523a363085fdcc
Manukumar N M <manunm.jps@gmail.com>
2023-01-07 20:56:01 +0530

    Update models.py

--- monitor/openwisp_monitoring/device/base/models.py
+++ monitor/openwisp_monitoring/device/base/models.py
@@ -262,7 +262,7 @@
    # @property
     def related_metrics(self):
         Metric = load_model('monitoring', 'Metric')
-        return Metric.objects.select_related('content_type').filter(
+        #return Metric.objects.select_related('content_type').filter(
            # object_id=self.device_id,
             #content_type__model='device',
             #content_type__app_label='config',
